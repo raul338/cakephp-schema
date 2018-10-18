f3e7ccd6c3f6925630ebc659fe1d67a425e9d116
Raúl Arellano <raul338@elhacker.net>
2018-10-18 12:03:32 -0300

    Use configurable seed file for fixtures

--- src/TestSuite/Fixture/SchemaFixture.php
+++ src/TestSuite/Fixture/SchemaFixture.php
@@ -9,8 +9,12 @@ use Cake\Utility\Inflector;
  */
 class SchemaFixture extends TestFixture
 {
-    public $fields = [];
-    public $records = [];
+    /**
+     * Seed file to use for the records
+     *
+     * @var string
+     */
+    public $seedFile = ROOT . DS . 'config' . DS . 'seed.php';
 
     /**
      * Workaround to use cakephp-schema seed as fixtures
@@ -26,8 +30,8 @@ class SchemaFixture extends TestFixture
         if (array_key_exists($className, $ret['tables'])) {
             $this->fields = $ret['tables'][$className];
         }
-        if (file_exists(ROOT . DS . 'config' . DS . 'seed.php')) {
-            $ret = require ROOT . DS . 'config' . DS . 'seed.php';
+        if (file_exists($this->seedFile)) {
+            $ret = require $this->seedFile;
             if (array_key_exists($className, $ret)) {
                 $this->records = $ret[$className];
             }
