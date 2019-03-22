197f8ee2c19d28508170caf37f676036dd8b81ea
Raúl Arellano <raul338@elhacker.net>
2019-03-22 17:46:05 -0300

    Use active connection to read tables

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,16 +1,28 @@
 <?php
 use Cake\Core\Configure;
+use Cake\Event\Event;
 use Cake\Event\EventManager;
 use Schema\Shell\Task\SchemaSaveTask;
 
 $configKey = 'Schema.autoSaveSchemaAfterMigrate';
 
 if (!Configure::check($configKey) || Configure::read($configKey) === true) {
-    EventManager::instance()->on('Migration.afterMigrate', function () {
+    EventManager::instance()->on('Migration.afterMigrate', function (Event $event) {
+        /** @var \Migrations\Command\Migrate */
+        $migration = $event->getSubject();
+        $manager = $migration->getManager();
+        if ($manager == null) {
+            return;
+        }
+        $input = $manager->getInput();
+        $connectionName = $input->getOption('connection');
+
         $task = new SchemaSaveTask;
         $task->interactive = false;
         $task->initialize();
         $task->loadTasks();
-        $task->save();
+        $task->save([
+            'connection' => $connectionName ?: 'default',
+        ]);
     });
 }
