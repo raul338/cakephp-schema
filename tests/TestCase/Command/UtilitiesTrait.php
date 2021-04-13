935fdecb3378fbdb6813740f0ddd37820ff2494a
Raúl Arellano <raul338@elhacker.net>
2021-04-13 18:40:29 -0300

    Use quotes to drop tables

--- tests/TestCase/Command/UtilitiesTrait.php
+++ tests/TestCase/Command/UtilitiesTrait.php
@@ -10,8 +10,10 @@ trait UtilitiesTrait
     protected function dropTables(): void
     {
         $conn = ConnectionManager::get('default');
-        $conn->execute('DROP TABLE IF EXISTS `users`');
-        $conn->execute('DROP TABLE IF EXISTS `profiles`');
-        $conn->execute('DROP TABLE IF EXISTS `phinxlog`');
+        $driver = $conn->getDriver()->quoteIdentifier('users');l
+        // $conn->getDriver()->quoteIdentifier('users');
+        $conn->execute('DROP TABLE IF EXISTS ' . $driver->quoteIdentifier('users'));
+        $conn->execute('DROP TABLE IF EXISTS ' . $driver->quoteIdentifier('profiles'));
+        $conn->execute('DROP TABLE IF EXISTS ' . $driver->quoteIdentifier('phinxlog')));
     }
 }
